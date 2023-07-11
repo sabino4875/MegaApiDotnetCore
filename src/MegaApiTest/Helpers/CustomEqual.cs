ab38c09d839a5cfe273619954119d9aff1183830
Sabino Ronaldo da Silva <sabino.silva@afixcode.com.br>
2023-07-10 21:36:47 -0300

    Adicionado projeto para testes das alterações feitas na API.

---
+++ src/MegaApiTest/Helpers/CustomEqual.cs
@@ -0,0 +1,27 @@
+﻿namespace MegaApiTest.Helpers
+{
+    using System;
+    public static class CustomEqual
+    {
+        public static void Validate<TAction>(TAction expected, TAction actual, String userMessage)
+        {
+            Boolean areEqual;
+
+            if (expected == null || actual == null)
+            {
+                // If either null, equal only if both null
+                areEqual = (expected == null && actual == null);
+            }
+            else
+            {
+                // expected is not null - so safe to call .Equals()
+                areEqual = expected.Equals(actual);
+            }
+
+            if (!areEqual)
+            {
+                throw new CustomEqualException(expected, actual, userMessage);
+            }
+        }
+    }
+}
